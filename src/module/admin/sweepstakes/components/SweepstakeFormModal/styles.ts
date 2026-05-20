3f6b9d92908e5dff949d3b64e0743c9e3f6ccfdf
odutradev <joao.vitornl@gmail.com>
2026-05-20 12:06:28 -0300

    fix: Add missing styles file to the bolão form modal

---
+++ src/module/admin/sweepstakes/components/SweepstakeFormModal/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+
+export const FormContainer = styled(Box)(({ theme }) => ({
+  flexDirection: 'column',
+  gap: theme.spacing(2),
+  display: 'flex',
+  marginTop: 8
+}))
