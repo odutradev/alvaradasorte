eb6caa34293a55a91d8a62ba895884ed35a1442f
odutradev <joao.vitornl@gmail.com>
2026-05-20 16:44:41 -0300

    refactor: Update toast configuration to centralize and follow system theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import 'react-toastify/dist/ReactToastify.css'
 import { RouterProvider } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { createRoot } from 'react-dom/client'
 import { StrictMode, useMemo } from 'react'
 
-import { toastContainerConfig } from '@config/toastConfig'
+import { getToastContainerConfig } from '@config/toastConfig'
 import useSystemStore from '@stores/system'
 import { getAppTheme } from '@theme/theme'
 import { appRouter } from './router'
@@ -14,11 +14,12 @@ const App = () => {
   const { themeMode } = useSystemStore((state) => state.system)
 
   const currentTheme = useMemo(() => getAppTheme(themeMode), [themeMode])
+  const toastConfig = useMemo(() => getToastContainerConfig(themeMode), [themeMode])
 
   return (
     <ThemeProvider theme={currentTheme}>
       <CssBaseline />
-      <ToastContainer {...toastContainerConfig} />
+      <ToastContainer {...toastConfig} />
       <RouterProvider router={appRouter} />
     </ThemeProvider>
   )
